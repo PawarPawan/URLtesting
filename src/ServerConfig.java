a40797babd290449583fea241e0b9bf6c780581d
Pawan Pawar <Pawar@users.noreply.github.com>
2015-07-28 23:46:08 +0530

    Update ServerConfig.java

--- src/ServerConfig.java
+++ src/ServerConfig.java
@@ -1,6 +1,13 @@
 import java.util.HashMap;
 
-
+/**
+ * @author Pawan Pawar
+ * @project URLTesting
+ * @package
+ * @fileName ServerConfig.java
+ * @date 15-Mar-2015
+ * @time 1:08:39 pm
+ */
 public class ServerConfig {
 	public static HashMap<String,String> config = new HashMap<String, String>();
 }
